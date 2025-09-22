dffad3cb7e37327038802839481b43ae518e4c1e
JesusPayan <jesus.he.payan@gmail.com>
2025-09-22 12:46:08 -0700

    fixing database conection

--- app/config.py
+++ app/config.py
@@ -1,8 +1,30 @@
+# import os
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = (
+#         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+#         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    )
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT")  # puede ser None
+    DB_NAME = os.getenv("DB_NAME", "crm_app_db")
+
+    if DB_PORT:
+        SQLALCHEMY_DATABASE_URI = (
+            f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = (
+            f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+        )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
